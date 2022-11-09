e76d1f06fc50cd2a966044f30dbab791263fa557
tunepi <91961866+tunepi@users.noreply.github.com>
2022-11-09 19:36:26 +0900

    ide-helperの導入とphpDocの記述

--- src/laravel/app/Models/GitOption.php
+++ src/laravel/app/Models/GitOption.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\GitOption
+ *
+ * @property int $id
+ * @property int $git_id
+ * @property string|null $git_option
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption query()
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereGitId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereGitOption($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GitOption whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class GitOption extends Model
 {
     use HasFactory;
